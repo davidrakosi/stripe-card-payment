77a8ff3517962ce6c5a3ea0aa465000a099f314b
Rafeh Qazi <rafehqazi@Rafehs-MacBook-Pro.local>
2020-06-13 15:02:36 -0700

    Accept stripe payments with card details.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Accept a Card Payment
+
+Build a simple checkout form to collect card details. Included are some basic build and run scripts you can use to start up the application.
+
+## Running the sample
+
+1. Build the application
+
+```npm install```
+
+ 2. Run the application
+
+```npm start```
+
+3. Go to [localhost:3000/](localhost:3000/)
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import "./App.css";
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// loadStripe is initialized with your real test publishable API key.
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+export default function App() {
+  return (
+    <div className="App">
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
+    </div>
+  );
+}
